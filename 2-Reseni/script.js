69d40c1e1d361037e4d1014c2fe840d945a2a48c
Kamil Ševeček <kamil@sevecek.net>
2019-08-14 14:55:15 +0200

    Zadani a reseni

---
+++ 2-Reseni/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var aktualniHrac;
+
+function priKliknutiNaBunku(event) {
+  // Nadefinuj prazdnou promennou elementPolicka
+  var znackaPolicka;
+
+  // Vloz do znackaPolicka bunku <td> z HTML, na kterou se kliknulo
+  znackaPolicka = event.target;
+
+  // Pouze pokud je vnitrni obsah elementPolicka prazdny
+  if (znackaPolicka.textContent === "") {
+    // Vloz do vnitrku bunky (tedy <td>SEM</td>) text z promenne aktualniHrac
+    znackaPolicka.textContent = aktualniHrac;
+
+    // Vymen hodnotu v aktualniHrac
+    if (aktualniHrac === "X") {
+      aktualniHrac = "O";
+    } else {
+      aktualniHrac = "X";
+    }
+  }
+}
